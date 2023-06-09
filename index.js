d8f1168497ba15a9780bf73e2a555bc75ceae1ad
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-09 00:03:37 -0600

    using array find function with an array of objects

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(arr)
+{
+    let item = arr.find(function(element) {
+        //console.log("element = " + element);
+        //console.log("element.result = " + element.result);
+        if (element.result === "W")
+        {
+            //console.log("WIN FOUND!");
+            return true;
+        }
+        else return false;
+    });
+    //console.log("item = " + item);
+    if (item == undefined) return undefined;
+    else return item.year;
+}
